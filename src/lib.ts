3ed017614d3925f248d61ea56e263c84287e0886
fletcherist <fletcherist@yandex.ru>
2020-09-30 22:13:22 +0300

    feat: better groups handling

--- src/lib.ts
+++ src/lib.ts
@@ -1,7 +1,6 @@
 type Pitch = 'c' | 'd' | 'e' | 'f' | 'g' | 'a' | 'b';
 type Duration = 1 | 2 | 4 | 8 | 16 | 32;
 type Time = '4/4' | '3/4';
-type NestedArray = Array<Note | Note[]>;
 
 interface Note {
   type: 'note';
@@ -20,9 +19,18 @@ interface Rest {
   duration: Duration;
 }
 
+type Modifier = 'default' | 'beam' | 'chord' | 'absolute' | 'relative';
+
+type Item = Group | Note | Rest;
 interface Group {
   type: 'group';
-  children: Array<Note | Group>;
+  children: Array<Note | Group | Rest>;
+  modifier: Modifier;
+}
+interface GroupChord {
+  type: 'group';
+  modifier: 'chord';
+  children: Note[];
 }
 
 export const note = (pitch: Pitch, duration: Duration): Note => {
@@ -69,8 +77,68 @@ export const dot = (note: Note): Note => {
 //     return note;
 //   });
 // };
-export const beam = (...notes: Note[]): Note[] => {
-  return notes.map((note, index) => {
+
+export const render = (group: Group): string => {
+  const renderNote = (note: Note): string => {
+    const renderPitch = (note: Note): string => {
+      if (note.sharp) {
+        return `${note.pitch}/-sharp`;
+      } else if (note.flat) {
+        return `${note.pitch}/-flat`;
+      }
+      return `${note.pitch}`;
+    };
+    const renderBeam = (note: Note): string => {
+      if (note.beamStart) {
+        return `(`;
+      } else if (note.beamEnd) {
+        return ')';
+      }
+      return '';
+    };
+    return [renderPitch(note), note.duration, renderBeam(note)].filter(Boolean).join('');
+  };
+
+  const renderGroup = (group: Group) => {
+    return group.children
+      .map((item) => {
+        if (item.type === 'note') {
+          return renderNote(item);
+        } else if (item.type === 'group') {
+          return render(item);
+        }
+      })
+      .join(' ');
+  };
+
+  if (group.modifier === 'default') {
+    return `${renderGroup(group)}`;
+  } else if (group.modifier === 'absolute') {
+    return `\\absolute { ${renderGroup(group)} }`;
+  } else if (group.modifier === 'relative') {
+    return `\\relative { ${renderGroup(group)} }`;
+  } else if (group.modifier === 'beam') {
+    return `${renderGroup(group)}`;
+  } else {
+    throw new Error(`Group modifier ${group.modifier} is not supported`);
+  }
+};
+
+const createGroup = (type: Modifier, ...children: Item[]): Group => {
+  return {
+    type: 'group',
+    modifier: type,
+    children: children,
+  };
+};
+
+// group without modifiers
+export const group = (...notes: Item[]): Group => {
+  return createGroup('default', ...notes);
+};
+
+export const beam = (...notes: Item[]): Group => {
+  const beamNotes = notes.map((note, index) => {
     const isFirstNote = index === 0;
     const isLastNote = index === notes.length - 1;
     if (isFirstNote) {
@@ -82,46 +150,24 @@ export const beam = (...notes: Note[]): Note[] => {
     }
     return note;
   });
-};
-
-const render = (...notes: Note[]): string => {
-  const renderPitch = (note: Note): string => {
-    if (note.sharp) {
-      return `${note.pitch}/-sharp`;
-    } else if (note.flat) {
-      return `${note.pitch}/-flat`;
-    }
-    return `${note.pitch}`;
+  return {
+    type: 'group',
+    children: beamNotes,
+    modifier: 'beam',
   };
-  const renderBeam = (note: Note): string => {
-    if (note.beamStart) {
-      return `(`;
-    } else if (note.beamEnd) {
-      return ')';
-    }
-    return '';
-  };
-  return notes
-    .map((note) => {
-      return [renderPitch(note), note.duration, renderBeam(note)].filter(Boolean).join('');
-    })
-    .join(' ');
 };
 
-export const absolute = (...notes: NestedArray): string => {
-  return `\\absolute { ${render(...notes.flat())} }`;
+export const absolute = (...notes: Item[]): Group => {
+  return createGroup('absolute', ...notes);
 };
 
-export const relative = (...notes: NestedArray): string => {
-  return `\\relative { ${render(...notes.flat())} }`;
+export const relative = (...notes: Item[]): Group => {
+  return createGroup('relative', ...notes);
 };
 
-export const group = (...notes: NestedArray): string => {
-  return render(...notes.flat());
-};
-
-export const chord = (...notes: NestedArray): string => {
-  return `<${notes.flat().join(' ')}>`;
+export const chord = (...notes: Note[]): Group => {
+  // return `<${notes.flat().join(' ')}>`;
+  return createGroup('chord', ...notes);
 };
 
 export const staff = (
